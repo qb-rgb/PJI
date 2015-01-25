5ac36e45760f74a2bc9eb41b9a72c9ad8704ba98
qw0te <baert.quentin@gmail.com>
2015-01-25 15:15:54 +0100

    Documentation de la classe PDFDownloader

--- src/main/scala/download/PDFDownloader.scala
+++ src/main/scala/download/PDFDownloader.scala
@@ -2,8 +2,16 @@ import sys.process._
 import java.net.URL
 import java.io.File
 
+/**
+ * Object to download a PDF from the french National Assembly website :
+ * http://archives.assemblee-nationale.fr/X/cri
+ * (with X the legislature number between 1 and 11)
+ *
+ * @author Quentin Baert
+ */
 object PDFDownloader {
 
+  // Gives the local path of the PDF from its URL
   private def pdfPath(url: String): String = {
     val parts: List[String] = (url split '/').toList
     val legislature = parts(3)
@@ -12,6 +20,12 @@ object PDFDownloader {
     "./cri/" + legislature + "/" + years + "/"
   }
   
+  /**
+   * Downloads a PDF from the given URL.
+   *
+   * @param url
+   *          URL of the PDF
+   */
   def downloadPDF(url: String): Unit = {
     val name = url substring ((url lastIndexOf '/') + 1)  
     val path = this.pdfPath(url)
