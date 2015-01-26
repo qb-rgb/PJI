fb47842f587b3216c18ec7541b572e5d2072e629
qw0te <baert.quentin@gmail.com>
2015-01-26 14:30:28 +0100

    Ajoute une classe afin de gerer les URLs

---
+++ src/main/scala/download/URLManager.scala
@@ -0,0 +1,45 @@
+import scala.io.Source
+
+object URLManager {
+
+  private val prefix = "http://archives.assemblee-nationale.fr"
+
+  val indexes = for (leg <- 1 to 11) yield (this.prefix + "/" + leg + "/cri/index.asp")
+
+  private def urlContentToString(url: String): String = {
+    val html = Source.fromURL(url, "iso-8859-1")
+    html.mkString
+  }
+
+  private def catchURL(
+    from: String,
+    linesFilter: (String) => (Boolean),
+    urlMapping: (String) => (String)): List[String] = {
+
+    val html = urlContentToString(from)
+    val linesToKeep = (html split '\n') filter linesFilter
+
+    (linesToKeep map urlMapping).toList
+  }
+
+  private def catchSessionURL(line: String): String =
+    line.substring((line indexOf "href=\"") + 6, (line indexOf "Compte rendu") - 2)
+
+  def sessionsURL(index: String): List[String] = {
+    this.catchURL(
+      index,
+      {x: String => x contains "Compte rendu"}, 
+      {x: String => this.prefix + this.catchSessionURL(x)})
+  }
+
+  private def catchPDFURL(line: String): String =
+    line.substring((line indexOf "href=\"") + 6, (line indexOf ".pdf") + 4)
+
+  def pdfURL(session: String): List[String] = {
+    this.catchURL(
+      session,
+      {x: String => x contains ".pdf"},
+      {x: String => this.prefix + "/" + (session charAt (this.prefix.length + 1)) + "/cri/" + this.catchPDFURL(x)})
+  }
+
+}
