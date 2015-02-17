67ea453391ecb796d6a13449090526253ee6e18b
qw0te <baert.quentin@gmail.com>
2015-02-17 23:46:02 +0100

    Ajout de la documentation

--- src/main/scala/textAnalysis/VotingFilter.scala
+++ src/main/scala/textAnalysis/VotingFilter.scala
@@ -1,8 +1,12 @@
 object VotingFilter {
 
+  /**
+   * List of all the local text paths
+   */
   val allLocalTextPaths: List[String] =
     PDFConverter.allLocalPDFsPaths map (_.replace("pdf", "txt"))
 
+  // Criterion from which filter the text files
   private def votingFilter(file: String): Boolean =
     (file contains "Annexe au procès-verbal") ||
     (file contains "Annexe au proces-verbal") ||
