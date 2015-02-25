00432be7de49ad6bcdc92712e71374dd0b29e06a
qw0te <baert.quentin@gmail.com>
2015-02-25 09:48:22 +0100

    Modifie la methode qui trouve tous les fichiers textes locaux
    À la place de partir de la base (les liens tirés du HTML) et de remonter
    toutes les modifications opérées pour retrouver les fichiers textes, la
    recherche est directement faite en local

--- src/main/scala/textAnalysis/VotingFilter.scala
+++ src/main/scala/textAnalysis/VotingFilter.scala
@@ -1,11 +1,29 @@
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
+import scala.io.Source
+
 object VotingFilter {
 
   /**
    * List of all the local text paths
    */
-  val allLocalTextPaths: List[String] =
-    PDFConverter.allLocalPDFsPaths map (_.replace("pdf", "txt"))
+  val allLocalTextPaths: List[String] = {
+    def findTxtFiles(root: File): List[String] = 
+      if (root.isFile)
+        if (root.getName endsWith "txt")
+          List(root.getCanonicalPath)
+        else
+          List()
+      else
+        (root.listFiles).toList flatMap findTxtFiles
 
+    val root = new File("./critxt")
+
+    findTxtFiles(root)
+  }
+  
   // Criterion from which filter the text files
   private def votingFilter(file: String): Boolean =
     (file contains "Annexe au procès-verbal") ||
