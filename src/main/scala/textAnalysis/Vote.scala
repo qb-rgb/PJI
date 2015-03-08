6b8291f7fa117b287d99ed691a04b10e5f2e6376
qw0te <baert.quentin@gmail.com>
2015-03-08 21:34:45 +0100

    Ajoute une classe pour représenter un scrutin

---
+++ src/main/scala/textAnalysis/Vote.scala
@@ -0,0 +1,27 @@
+/**
+ * Represents a vote
+ *
+ * @constructor creates a new vote
+ * @param date date of the vote
+ * @param number number of the vote
+ * @param subject subject of the vote
+ * @param voterNb number of voter
+ * @param expressedVotes number of suffrage that are expressed
+ * @param absoluteMajority number of suffrage necessary to make the vote
+ *                         effective
+ * @param forNb number of suffrage expressed for the vote
+ * @param againstNb number of suffrage against the vote
+ * @param voteAnalyse analyse of the vote
+ *
+ * @author Quentin Baert
+ */
+class Vote(
+  val date: Date,
+  val number: Int,
+  val suject: String,
+  val voterNb: Int,
+  val expressedVotes: Int,
+  val absoluteMajority: Int,
+  val forNb: Int,
+  val againstNb: Int,
+  val voteAnalyse: String)
