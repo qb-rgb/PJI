d15cbaf555906a72d857556fa49f732aeaf61b18
qw0te <baert.quentin@gmail.com>
2015-03-25 14:16:31 +0100

    Renforce quelques pattern
    Prise en compte des accents et insensibilité à la casse

--- src/main/scala/utils/PatternDictionnary.scala
+++ src/main/scala/utils/PatternDictionnary.scala
@@ -45,25 +45,37 @@ object PatternDictionnary {
    * Pattern to find the number of voters
    */
   val nbOfVotersPattern: Pattern =
-    Pattern compile (this buildRecapString "Nombre de votants")
+    Pattern.compile(
+      this buildRecapString "Nombre de votants",
+      Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE
+    )
 
   /**
    * Pattern to find the number of expressed votes
    */
   val nbOfExpressedVotesPattern: Pattern =
-    Pattern compile (this buildRecapString "Nombre de suffrages exprimés")
+    Pattern.compile(
+      this buildRecapString "Nombre de suffrages exprim[ée]s",
+      Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE
+    )
 
   /**
    * Pattern to find the absolute majority
    */
   val absoluteMajorityPattern: Pattern =
-    Pattern compile (this buildRecapString "Majorité absolue")
+    Pattern.compile(
+      this buildRecapString "Majorit[ée] absolue",
+      Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE
+    )
 
   /**
    * Pattern to find the for votes number
    */
   val forNbPattern: Pattern =
-    Pattern compile (this buildRecapString "Pour l’adoption")
+    Pattern.compile(
+      this buildRecapString "Pour l’adoption",
+      Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE
+    )
 
   /**
    * Pattern to find the against votes number
