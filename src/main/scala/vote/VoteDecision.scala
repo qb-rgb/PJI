8c33a170a68103947b49926c6cdabe6bfc151062
qw0te <baert.quentin@gmail.com>
2015-04-27 23:16:24 +0200

    Ajoute les méthodes toString() aux différentes décisions de votes

--- src/main/scala/vote/VoteDecision.scala
+++ src/main/scala/vote/VoteDecision.scala
@@ -4,6 +4,21 @@
  * @author Quentin Baert
  */
 trait VoteDecision
-object For extends VoteDecision
-object Against extends VoteDecision
-object Abstention extends VoteDecision
+
+object For extends VoteDecision {
+
+  override def toString: String = "For"
+
+}
+
+object Against extends VoteDecision {
+
+  override def toString: String = "Against"
+
+}
+
+object Abstention extends VoteDecision {
+
+  override def toString = "Abstention"
+
+}
